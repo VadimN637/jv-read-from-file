01a9f6a64f5440b782e1f919ed118ef0267deaf1
VadimN637 <turnedofff@gmail.com>
2026-03-27 19:55:52 +0200

    1

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,28 @@
 package core.basesyntax;
 
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        String[] split = fileName.split("\\W+");
+        for (int i = 0; i < split.length; i++) {
+            split[i] = split[i].toLowerCase();
+        }
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < split.length; i++) {
+            if (split[i].startsWith("w")) {
+                result.add(split[i]);
+            }
+        }
+        Collections.sort(result);
+        if (!result.isEmpty()) {
+            return result.toArray(new String[0]);
+        } else {
+            return null;
+        }
     }
 }
